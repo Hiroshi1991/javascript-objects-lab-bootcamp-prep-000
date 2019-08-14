61dfe959f461627dc6d13e78eb84f58b9bea9d1e
Hiroshi S <sipahioglu_7@hotmail.com>
2019-08-14 15:10:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,3 @@ function updateObjectWithKeyAndValue(object, key, value){
   return Object.assign ({}, object, {[key] : value})
 }
 
-function updateObjectWithKeyAndValue(object, key, value){
-  object[key]=value
-  return object
-}
